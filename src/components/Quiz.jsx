5256c5edec909e0acca0a05a67c97a1843ff9bed
vitorfarias1 <vitor.rodriguesfarias@gmail.com>
2026-02-07 19:43:10 -0300

    wip: improve quiz component to show questions and answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,47 @@
 import { useState } from "react"
+import QUESTIONS from "../questions";
+
+import quizCompletedImg from "../assets/quiz-complete.png"
 
 export default function Quiz() {
     
     const [userAnswer, setUserAnswer] = useState([]);
 
     const activeQuestionIndex = userAnswer.length;
+    
+    const isQuizCompleted = activeQuestionIndex === QUESTIONS.length;
+    
+    
 
-    return <p>Some Question here!</p>
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswer((prevAnswers) => {
+            return [...prevAnswers, selectedAnswer]
+        })
+    }
+
+    if (isQuizCompleted) {
+        return <div id="summary">
+                <img src={quizCompletedImg} alt="quiz completed img"/>
+                <h2>Quiz Completed!</h2>
+            </div>
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+    
+
+    return (
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">{shuffledAnswers.map(answer => (
+                    <li key={answer} className="answer">
+                        <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                    </li>
+                ))}
+                </ul>
+            </div>
+            
+        </div>
+    );
 }
