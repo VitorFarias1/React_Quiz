db991a6e0df55fe211103353b0ddc5d934d0a791
vitorfarias1 <vitor.rodriguesfarias@gmail.com>
2026-02-16 11:11:45 -0300

    wip: create summary component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,7 @@
 import { useState, useCallback } from "react"
 import QUESTIONS from "../questions";
-
-import quizCompletedImg from "../assets/quiz-complete.png"
 import Question from "./Question";
+import Summary from "./Summary";
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
@@ -20,10 +19,7 @@ export default function Quiz() {
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
     if (isQuizCompleted) {
-        return <div id="summary">
-                <img src={quizCompletedImg} alt="quiz completed img"/>
-                <h2>Quiz Completed!</h2>
-            </div>
+        return <Summary userAnswers={userAnswers}/>
     }    
 
     return (
